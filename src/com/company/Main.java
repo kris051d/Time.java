84af3ca76aac219519f43123c0046ebda29f01ff
kris051d <kris051d@edu.easj.dk>
2021-09-07 10:22:31 +0200

    Forkortet koden

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,20 +6,15 @@ public class Main {
         int hour = 18;
         int minute = 43;
         int second = 42;
-        System.out.print("Number of seconds since midnight: ");
-        System.out.println(hour*60*60+minute*60+second);
+        System.out.println("Number of seconds since midnight: "+(hour*60*60+minute*60+second));
         int SinceMidnight = (hour*60*60+minute*60+second);
         int SecondsDay = 24*60*60;
-        System.out.print("Number of seconds remaining in the day: ");
-        System.out.println(SecondsDay-SinceMidnight);
-        System.out.print("Percent of the day that has passed: ");
-        System.out.println(SinceMidnight*100/SecondsDay);
+        System.out.println("Number of seconds remaining in the day: "+(SecondsDay-SinceMidnight));
+        System.out.println("Percent of the day that has passed: "+(SinceMidnight*100/SecondsDay));
         int hourEnd = 19;
         float minuteEnd = 31;
         float secondEnd = 14;
-        System.out.print("Seconds spend doing this exersice: ");
-        System.out.println(hourEnd*60*60+minuteEnd*60+secondEnd-SinceMidnight);
-        System.out.print("Percent of the day spent doing this exersice. ");
-        System.out.print((hourEnd*60*60+minuteEnd*60+secondEnd-SinceMidnight)*100/SecondsDay);
+        System.out.println("Seconds spend doing this exersice: "+(hourEnd*60*60+minuteEnd*60+secondEnd-SinceMidnight));
+        System.out.print("Percent of the day spent doing this exersice. "+((hourEnd*60*60+minuteEnd*60+secondEnd-SinceMidnight)*100/SecondsDay));
     }
 }
